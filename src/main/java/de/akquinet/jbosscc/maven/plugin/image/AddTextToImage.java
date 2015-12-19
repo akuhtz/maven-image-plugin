e283f48d3d8c8bf3dac4a68b6df8c4cfbe57b94c
Andreas Kuhtz <andreas.kuhtz@gmail.com>
2015-12-19 17:08:43 +0100

    Allow to specify right aligned text with a negative number for offsetX.

--- src/main/java/de/akquinet/jbosscc/maven/plugin/image/AddTextToImage.java
+++ src/main/java/de/akquinet/jbosscc/maven/plugin/image/AddTextToImage.java
@@ -1,84 +1,104 @@
 package de.akquinet.jbosscc.maven.plugin.image;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
+import javax.imageio.ImageIO;
+
 /**
  * Tool to add some text to an Image at specified positions with a specified font and color.
  */
-public final class AddTextToImage
-{
-  private BufferedImage image;
-  private Graphics graphics;
-  private int imageHeight;
-  private String imageFormat = "jpg";
+public final class AddTextToImage {
+    private BufferedImage image;
 
-  private AddTextToImage()
-  {
-    // use factory method
-  }
+    private Graphics2D graphics;
 
-  public static AddTextToImage create()
-  {
-    return new AddTextToImage();
-  }
+    private int imageHeight;
 
-  public AddTextToImage setImageFormat(final String imageFormat)
-  {
-    this.imageFormat = imageFormat;
-    return this;
-  }
+    private int imageWidth;
 
-  public void saveImage(final String targetImg) throws IOException
-  {
-    graphics.dispose();
-    ImageIO.write(image, imageFormat, new File(targetImg));
-  }
+    private String imageFormat = "jpg";
 
-  public AddTextToImage addText(final String text, final Font font, final Color color, final int offsetX,
-                                final int offsetY)
-  {
-    System.out.println(
-      "Adding Text: text=" + text + ", font=" + font + ", color=" + color + ", offsetX=" + offsetX + ", offsetY="
-        + offsetY);
+    private AddTextToImage() {
+        // use factory method
+    }
 
-    graphics.setFont(font);
-    graphics.setColor(color);
-    graphics.drawString(text, offsetX, imageHeight - font.getSize() / 2 - offsetY);
-    return this;
-  }
+    public static AddTextToImage create() {
+        return new AddTextToImage();
+    }
 
-  public AddTextToImage openImage(final String sourceImagePath) throws IOException
-  {
-    File file = new File(sourceImagePath);
-    Image srcImage = ImageIO.read(file);
-    final int imageWidth = srcImage.getWidth(null);
-    imageHeight = srcImage.getHeight(null);
-    image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
-    graphics = image.createGraphics();
-    graphics.drawImage(srcImage, 0, 0, imageWidth, imageHeight, null);
+    public AddTextToImage setImageFormat(final String imageFormat) {
+        this.imageFormat = imageFormat;
+        return this;
+    }
 
-    return this;
-  }
+    public void saveImage(final String targetImg) throws IOException {
+        graphics.dispose();
+        ImageIO.write(image, imageFormat, new File(targetImg));
+    }
 
-  public static void main(String[] args) throws IOException
-  {
-/*
-    String srcPath = "C:\\Projekte\\ubi-root\\vs\\vs-config\\vs-config-client-packaging\\src\\main\\resources-binary\\splash.jpg";
-    String targetPath = "C:\\Temp\\splash.png";
+    public AddTextToImage addText(
+        final String text, final Font font, final Color color, final int offsetX, final int offsetY) {
+        System.out.println("Adding Text: text=" + text + ", font=" + font + ", color=" + color + ", offsetX=" + offsetX
+            + ", offsetY=" + offsetY);
 
-    AddTextToImage.create()
-      .setImageFormat("png")
-      .openImage(srcPath)
-      .addText("UBI ist toll", new Font("Arial", Font.BOLD, 16), Color.WHITE, 50, 500)
-      .addText("UBI ist gut", new Font("Arial", Font.BOLD, 16), Color.WHITE, 50, 475)
-      .saveImage(targetPath);
-*/
+        graphics.setFont(font);
+        graphics.setColor(color);
 
-    System.out.println(Arrays.asList(GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts()));
-  }
+        int xPos = offsetX;
+        if (offsetX < 0) {
+            // make the text right-aligned
+
+            // get metrics from the graphics
+            FontMetrics metrics = graphics.getFontMetrics(font);
+            // get the height of a line of text in this
+            // font and render context
+            int hgt = metrics.getHeight();
+            // get the advance of my text in this font
+            // and render context
+            int adv = metrics.stringWidth(text);
+
+            // because offsetX is negative the value is subtracted
+            xPos = imageWidth + offsetX - adv;
+            System.out.println("The offsetX value is expected as right aligned value, new xPos: " + xPos
+                + ", calculated width of text: " + adv);
+        }
+
+        graphics.drawString(text, xPos, imageHeight - font.getSize() / 2 - offsetY);
+        return this;
+    }
+
+    public AddTextToImage openImage(final String sourceImagePath) throws IOException {
+        File file = new File(sourceImagePath);
+        Image srcImage = ImageIO.read(file);
+        imageWidth = srcImage.getWidth(null);
+        imageHeight = srcImage.getHeight(null);
+        image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
+        graphics = image.createGraphics();
+        graphics.drawImage(srcImage, 0, 0, imageWidth, imageHeight, null);
+
+        return this;
+    }
+
+    public static void main(String[] args) throws IOException {
+        /*
+         * String srcPath =
+         * "C:\\Projekte\\ubi-root\\vs\\vs-config\\vs-config-client-packaging\\src\\main\\resources-binary\\splash.jpg";
+         * String targetPath = "C:\\Temp\\splash.png";
+         * 
+         * AddTextToImage.create() .setImageFormat("png") .openImage(srcPath) .addText("UBI ist toll", new Font("Arial",
+         * Font.BOLD, 16), Color.WHITE, 50, 500) .addText("UBI ist gut", new Font("Arial", Font.BOLD, 16), Color.WHITE,
+         * 50, 475) .saveImage(targetPath);
+         */
+
+        System.out.println(Arrays.asList(GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts()));
+    }
 }
